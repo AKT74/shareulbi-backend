c8cd082f9b6d1c5ccb51e141acf45d2262f9190d
AKT74 <nayakat74@gmail.com>
2026-02-02 02:11:53 +0700

    adjust app.js

--- src/app.js
+++ src/app.js
@@ -30,9 +30,11 @@ const worksRoutes = require("./routes/works.routes")
 app.use(cors({
   origin: [
     "http://localhost:3000", 
-    "https://shareulbi-frontend.vercel.app/" // domain FE kamu
-  ], 
-  credentials: true
+     "https://shareulbi-frontend.vercel.app"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(express.json())
